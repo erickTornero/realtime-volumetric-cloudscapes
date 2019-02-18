d08235b52c3c2adf5b8ff7d424322b4cf04d0533
erickTorneroT <erickdeivy01@gmail.com>
2019-02-18 18:13:14 -0500

    base raymarching added

---
+++ shaders/RayMarchingFragment.glsl
@@ -0,0 +1,41 @@
+#version 330
+
+out vec4 color;
+
+uniform vec3 cameraPosition;
+uniform vec3 cameraFront;
+uniform vec3 cameraUp;
+uniform vec3 cameraRight;
+
+uniform float screenWidth;
+uniform float screenHeight;
+
+// Variable for sample textures
+uniform sampler3D lowFrequencyTexture;
+
+struct Ray{
+    vec3 origin;
+    vec3 direction;
+};
+vec3 GetRayDirection(vec3 front, vec3 right, vec3 up, float x, float y){
+    vec3 ray = 10 * front + right * x + up * y;
+    return normalize(ray);
+}
+float getHeightInAtmosphere()
+
+vec4 RayMarching(vec3 rayOrigin, vec3 rayDirection, vec3 earthCenter, float start_atm, float end_atm){
+    const float atmosphereThickness = end_atm - start_atm;
+    //TODO: Check if maxNSteps is Ok!
+    const float maxNSteps = 200.0;
+    const float incrementAtm = atmosphereThickness/maxNSteps;
+
+    vec3 colorPixel = vec3(0.0);
+    // Start Ray marching
+    for(float t = start_atm; t < end_atm; t += incrementAtm){
+        vec3 colorSample = vec3(0.0);
+
+        // Compute position
+        vec3 pos = rayOrigin + t * rayDirection;
+
+    }
+}
